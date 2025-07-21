a4c7dba735874dcb15fc8545a6017db2584599dc
Eduardo Locatelli <edu.locatelli2004@gmail.com>
2025-07-21 11:02:06 -0300

    Add files via upload

---
+++ exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "Hcode";
+//cria uma variavel chamada nome com o valor hcode
+//echo $nome;
+
+var_dump($nome);
+/*comando var_dump: mostra o tipo de dado
+                    o tamanho do texto
+                    e o conteudo da variavel*/
+?>
