4ebdfd04aac59533e85e531b351b756ea0262003
Eduardo Locatelli <edu.locatelli2004@gmail.com>
2025-07-22 14:13:01 -0300

    Add files via upload

---
+++ constante/exemplo-01.php
@@ -0,0 +1,8 @@
+<?php 
+
+define("SERVIDOR", "127.0.0.1");
+//define cria uma constante.
+//SERVIDOR é o nome da constante e o ip ali é o valor.
+echo SERVIDOR;
+
+?>
---
+++ constante/exemplo-03.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo PHP_VERSION;
+//constante padrão do PHP que mostra a versão do PHP
+echo "<br>";
+
+echo DIRECTORY_SEPARATOR
+/*constante padrão do PHP que mostra o tipo de barra
+usada nos caminhos de pastas(diretorios), dependendo 
+do sistema operacional.
+Windows \
+Linux e Mac / */
+?>
